eb1d6621f3a5b7b496930d2a1f088f3e689295b6
TheError133 <theerror133@gmail.com>
2015-11-13 20:04:17 +0300

    v.0.8 Первая версия. Реализована отправка файлов по 5 шт. на почтовый ящик gvc6@russianpst.ru. Пока что только основной функционал, но проверен.

---
+++ AutoSender/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AutoSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblFilesCount = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(486, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // lblFilesCount
+            // 
+            this.lblFilesCount.AutoSize = true;
+            this.lblFilesCount.Location = new System.Drawing.Point(25, 41);
+            this.lblFilesCount.Name = "lblFilesCount";
+            this.lblFilesCount.Size = new System.Drawing.Size(57, 13);
+            this.lblFilesCount.TabIndex = 1;
+            this.lblFilesCount.Text = "Найдено: ";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(28, 70);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(183, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(25, 109);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(0, 13);
+            this.lblAmount.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 261);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblFilesCount);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Отправка файлов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Label lblFilesCount;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblAmount;
+    }
+}
+
---
+++ AutoSender/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace AutoSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists("ToLoad") && Directory.Exists("Loaded"))
+            {
+                lblFilesCount.Text = "Найдено: " + (Directory.GetFiles("ToLoad").Count()).ToString();
+                if (Directory.GetFiles("ToLoad").Count() > 0)
+                    btnSend.Enabled = true;
+            }
+            else
+                if (!Directory.Exists("ToLoad"))
+                {
+                    lblFilesCount.Text = "Отсутствует папка \"ToLoad\"";
+                    return;
+                }
+                else
+                {
+                    lblFilesCount.Text = "Отсутствует папка \"Loaded\"";
+                    return;
+                }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            await Task.Factory.StartNew(() =>
+                {
+                    Sending();
+                }
+                );
+            lblAmount.Text = "Осталось: 0";
+        }
+        private void Sending()
+        {
+            Boolean b = false;
+            try
+            {
+                while (Directory.GetFiles("ToLoad").Count() > 0)
+                {
+                    if (!b)
+                    {
+                        b = true;
+                        FileInfo Results = new FileInfo("Loaded\\FilesList.txt");
+                        using (StreamWriter SW = new StreamWriter(Results.FullName))
+                            foreach (string filename in Directory.GetFiles("ToLoad"))
+                            {
+                                SW.WriteLine("'" + (new FileInfo(filename)).Name + "',");
+                            }
+                    }
+                    if (Directory.GetFiles("ToLoad").Count() > 5)
+                    {
+                        string[] FileNames = Directory.GetFiles("ToLoad");
+                        lblAmount.BeginInvoke(new Action(() => lblAmount.Text = "Осталось: " + FileNames.Length));
+                        MailMessage mail = new MailMessage();
+                        mail.From = new MailAddress("niips.support@yandex.ru");
+                        mail.To.Add(new MailAddress("gvc6@russianpost.ru"));
+                        mail.Subject = "autosender";
+                        mail.Body = "";
+                        for (int i = 0; i < 5; i++)
+                            mail.Attachments.Add(new Attachment(FileNames[i]));
+                        SmtpClient client = new SmtpClient();
+                        client.Host = "smtp.yandex.ru";
+                        client.Port = 587;
+                        client.EnableSsl = true;
+                        client.Credentials = new NetworkCredential("niips.support", "Niips2015");
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.Send(mail);
+                        mail.Dispose();
+                        for (int i = 0; i < 5; i++)
+                        {
+                            FileInfo FI = new FileInfo(FileNames[i]);
+                            FI.MoveTo("Loaded\\" + FI.Name);
+                        }
+                    }
+                    else
+                    {
+                        string[] FileNames = Directory.GetFiles("ToLoad");
+                        lblAmount.BeginInvoke(new Action(() => lblAmount.Text = "Осталось: " + FileNames.Length));
+                        MailMessage mail = new MailMessage();
+                        mail.From = new MailAddress("niips.support@yandex.ru");
+                        mail.To.Add(new MailAddress("gvc6@russianpost.ru"));
+                        mail.Subject = "autosender";
+                        mail.Body = "";
+                        for (int i = 0; i < FileNames.Length; i++)
+                            mail.Attachments.Add(new Attachment(FileNames[i]));
+                        SmtpClient client = new SmtpClient();
+                        client.Host = "smtp.yandex.ru";
+                        client.Port = 587;
+                        client.EnableSsl = true;
+                        client.Credentials = new NetworkCredential("niips.support", "Niips2015");
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.Send(mail);
+                        mail.Dispose();
+                        for (int i = 0; i < FileNames.Length; i++)
+                        {
+                            FileInfo FI = new FileInfo(FileNames[i]);
+                            FI.MoveTo("Loaded\\" + FI.Name);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
